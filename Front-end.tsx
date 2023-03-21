f859562bf4b19fbb1aa7537ae6e31c942e567102
Anderson Sousa de Lima <20221370042@ad.ifpb.edu.br>
2023-03-21 17:26:50 -0300

    front-end-conect

---
+++ Front-end.tsx
@@ -0,0 +1,31 @@
+// react app para consumir o back-end
+
+// estudando requisições http para api´s rest
+
+
+import axios from "axios"
+
+
+const Button = styled.button``
+
+
+const handleDelete = async (itemParaDeletar) => {
+    await axios
+    .delete("urldoprojeto//000" + arg "opcional!")
+    .then(({data})) => {
+        const newArray =user.filter((user)) => User.id !== id); 
+    }
+// instância "then" vai executar a condição a partir da sua requisição
+// a função handdelete , executa o (delete) 
+}
+
+
+
+    <Button onclick={() => handleDelete(itemParaDeletar.Arg) }>
+
+    </Button>
+
+
+// praticando o useffect 
+
+() useffect
