6a00487100a50c9a9638e611cde358e55e568a93
Anderson Lima <105503214+Andersonlima13@users.noreply.github.com>
2023-03-28 11:27:59 -0300

    alterar-component

--- Reducer-react.tsx
+++ Reducer-react.tsx
@@ -1,1 +1,17 @@
+// useSelector para acessar um dado , o mesmo deve trocar de componente
 
+import { useSelector } from "react-redux" 
+
+// definimos os nosso Userredux , e posteriormente a rota do objeto com o rootReducer
+// exemplo : /produtos/reducer
+
+//logo apos , o objeto/dado a ser modificado (cartReducer)
+
+const { products} = useSelector(rootReducer => rootReducer.cartReducer )
+
+
+// para o objeto iremos associa-lo ao componente que devemos mudar o estado 
+// ultilizaremos a função map , para cada item adcionado 
+
+
+{ products.map (product => <component id={product})}
